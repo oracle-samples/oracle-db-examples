f72c9b0b1c4a080160b0ad0d0b36cb64125310d2
Kuassim <kuassi.mensah@oracle.com>
2017-03-26 11:40:13 -0700

    Add files via upload

---
+++ java/ojvm/TrimLob.java
@@ -0,0 +1,112 @@
+/*
+/------------------------------------------------------------------------------
+/ Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+/
+/ Portions Copyright 2006-2015, Kuassi Mensah. All rights reserved.
+/
+/
+/------------------------------------------------------------------------------
+/ DESCRIPTION
+/ This sample shows basic BLOB/CLOB operations. It drops, creates, and populates a basic_lob_table
+/ with blob, clob data types columns then fetches the rows and trim both LOB and CLOB
+*/
+
+// Need to import the java.sql package to use JDBC
+import java.sql.*;
+
+/* 
+ * Need to import the oracle.sql package to use 
+ * oracle.sql.BLOB
+ */
+import oracle.sql.*;
+
+public class TrimLob
+{
+  public static void main (String args []) throws SQLException {
+  Connection conn;
+ /*
+  * Where is your code running: in the database or outside?
+  */
+  if (System.getProperty("oracle.jserver.version") != null)
+  {
+  /* 
+   * You are in the database, already connected, use the default 
+   * connection
+   */
+  conn = DriverManager.getConnection("jdbc:default:connection:");
+  }
+  else
+  {
+  /* 
+   * You are not in the database, you need to connect to 
+   * the database
+   */
+
+   DriverManager.registerDriver(new oracle.jdbc.OracleDriver());  
+   conn = 
+         DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522/orcl", "scott",
+          "tiger");
+  }
+  long t0,t1;
+   /* 
+    * auto commit is off (not suported)by default in OracleJVM
+    * It's faster with JDBC when auto commit is off
+    */
+    conn.setAutoCommit (false);
+    t0=System.currentTimeMillis(); 
+    // Create a Statement
+    Statement stmt = conn.createStatement ();
+    // Make sure the table is empty
+    stmt.execute("delete from basic_lob_table");
+    stmt.execute("commit"); 
+
+    // Populate the table
+    stmt.execute ("insert into basic_lob_table values ('first', " +
+                  "'010101010101010101010101010101', " +
+                  "'one.two.three.four.five.six.seven')");
+    stmt.execute ("insert into basic_lob_table values ('second', " +
+                  "'0202020202020202020202020202020202020202', " +
+                  "'two.three.four.five.six.seven.eight.nine.ten')");
+    
+   /* 
+    * Retive Lobs and modify contents; this can be done by doing
+    * "select ... for update", but "autocommit" is turned off and
+    * the previous "create table" statement already locks the table 
+    */
+    ResultSet rset = stmt.executeQuery
+                          ("select * from basic_lob_table for update");                   
+    while (rset.next ())
+    {
+      // Get the lobs
+      BLOB blob = (BLOB) rset.getObject (2);
+      CLOB clob = (CLOB) rset.getObject (3);
+
+      // Show the original lob length
+      System.out.println ("Show the original lob length");
+      System.out.println ("blob.length()="+blob.length());
+      System.out.println ("clob.length()="+clob.length());
+
+      // Trim the lobs
+      System.out.println ("Trim the lob to legnth = 6");
+      blob.truncate (6);
+      clob.truncate (6);
+
+      // Show the lob length after trim()
+      System.out.println ("Show the lob length after trim()");
+      System.out.println ("blob.length()="+blob.length());
+      System.out.println ("clob.length()="+clob.length());
+    }
+
+    // Close the ResultSet and Commit changes
+    rset.close ();
+    stmt.execute("commit"); 
+
+    // Close the Statement 
+    stmt.close ();
+  
+    t1=System.currentTimeMillis(); 
+    System.out.println ("====> Duration: "+(int)(t1-t0)+ "Milliseconds");
+    // Close the connection
+    conn.close ();
+  }
+}
---
+++ java/ojvm/TrimLobSp.sql
@@ -0,0 +1,134 @@
+create or replace java source named TrimLob as
+/*
+ * This SQL script generated a Java code performing BLOB/CLOB operations.
+ * The goal is to show Java in the database and contrat its performance with the same code
+ * running as a client-side or stand-alone JDBC code.
+ */
+
+// You need to import the java.sql package to use JDBC
+import java.sql.*;
+
+/* 
+ * You need to import the oracle.sql package to use 
+ * oracle.sql.BLOB
+ */
+import oracle.sql.*;
+
+public class TrimLob
+{
+  public static void main (String args []) throws SQLException {
+  Connection conn;
+ /*
+  * Where is your code running: in the database or outside?
+  */
+  if (System.getProperty("oracle.jserver.version") != null)
+  {
+  /* 
+   * You are in the database, already connected, use the default 
+   * connection
+   */
+  conn = DriverManager.getConnection("jdbc:default:connection:");
+  }
+  else
+  {
+  /* 
+   * You are not in the database, you need to connect to 
+   * the database
+   */
+
+   DriverManager.registerDriver(new oracle.jdbc.OracleDriver());  
+   conn = 
+         DriverManager.getConnection("jdbc:oracle:thin:", "scott",
+          "tiger");
+  }
+  long t0,t1;
+   /* 
+    * auto commit is off by default in OracleJVM (not supported)
+    * It's faster with JDBC when auto commit is off
+    */
+    conn.setAutoCommit (false);
+    t0=System.currentTimeMillis(); 
+    // Create a Statement
+    Statement stmt = conn.createStatement ();
+
+    // clean up 
+     try
+    {
+      stmt.execute ("drop table basic_lob_table");
+    }
+    catch (SQLException e)
+    {
+      // An exception could be raised here if the
+      // table did not exist already.
+    }
+
+    // Create a table containing a BLOB and a CLOB
+    stmt.execute ("create table basic_lob_table (x varchar2 (30), " + 
+                                 "b blob, c clob)");
+    // Populate the table
+    stmt.execute ("insert into basic_lob_table values ('first', " +
+                  "'010101010101010101010101010101', " +
+                  "'one.two.three.four.five.six.seven')");
+    stmt.execute ("insert into basic_lob_table values ('second', " +
+                  "'0202020202020202020202020202020202020202', " +
+                  "'two.three.four.five.six.seven.eight.nine.ten')");
+    
+   /* 
+    * Retive Lobs and modify contents; this can be done by doing
+    * "select ... for update", but "autocommit" is turned off and
+    * the previous "create table" statement already locks the table 
+    */
+    ResultSet rset = stmt.executeQuery
+                          ("select * from basic_lob_table");                   
+    while (rset.next ())
+    {
+      // Get the lobs
+      BLOB blob = (BLOB) rset.getObject (2);
+      CLOB clob = (CLOB) rset.getObject (3);
+
+      // Show the original lob length
+      System.out.println ("Show the original lob length");
+      System.out.println ("blob.length()="+blob.length());
+      System.out.println ("clob.length()="+clob.length());
+
+      // Trim the lobs
+      System.out.println ("Trim the lob to legnth = 6");
+      blob.trim (6);
+      clob.trim (6);
+
+      // Show the lob length after trim()
+      System.out.println ("Show the lob length after trim()");
+      System.out.println ("blob.length()="+blob.length());
+      System.out.println ("clob.length()="+clob.length());
+    }
+
+    // Close the ResultSet
+    rset.close ();
+
+    // Close the Statement 
+    stmt.close ();
+  
+    t1=System.currentTimeMillis(); 
+    System.out.println ("====> Duration: "+(int)(t1-t0)+ "Milliseconds");
+    // Close the connection
+    conn.close ();
+  }
+}
+/
+
+show errors;
+
+alter java source TrimLob compile;
+
+show errors;
+
+create or replace procedure TrimLobSp as 
+ language java name 'TrimLob.main(java.lang.String[])';
+/
+
+show errors;
+set serveroutput on 
+call dbms_java.set_output(50000);
+ 
+call TrimLobSp();
+
