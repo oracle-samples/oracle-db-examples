594171dc2d11bab07f0753283e887b4a7758dde3
Kuassim <kuassi.mensah@oracle.com>
2017-04-06 09:10:55 -0700

    Update hello.js

--- javascript/nashorn/hello.js
+++ javascript/nashorn/hello.js
@@ -2,6 +2,11 @@ function hello()
 {
 /*
 *This is a sample Javascript file that prints "Hello World".
+*
+* Before invoking it, you must load it into your database user schema
+* loadjava –v –r –u hr/hr hello.js
+* then call hello.js from SQL*Plus
+* SQL> @hello.sql
 */
 var hellow = "Hello World";
 return hellow;
