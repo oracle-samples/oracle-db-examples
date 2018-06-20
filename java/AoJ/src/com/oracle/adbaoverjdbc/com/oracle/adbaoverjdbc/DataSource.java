c096939fb8f65f376682bd52c87dc74baafb7990
Kuassim <kuassi.mensah@oracle.com>
2018-06-20 15:54:00 -0700

    Clean up and re-upload

--- java/AoJ/src/com/oracle/adbaoverjdbc/com/oracle/adbaoverjdbc/DataSource.java
+++
@@ -1,5 +0,0 @@
-<html>
-<head><title> Oracle SSO Failure</title></head>
- <body bgcolor="white"> <font color="red">
- <h1>Oracle SSO Failure - Unable to process request</h1> </font> Either the requested URL was not specified in terms of a fully-qualified host name or OHS single sign-on is incorrectly configured.<br> Please notify your administrator.<hr> </body>
-</html>
--- java/AoJ/test/com/oracle/adbaoverjdbc/test/NewEmptyJUnitTest.java
+++ java/AoJ/test/com/oracle/adbaoverjdbc/test/NewEmptyJUnitTest.java
@@ -1,5 +1,55 @@
-<html>
-<head><title> Oracle SSO Failure</title></head>
- <body bgcolor="white"> <font color="red">
- <h1>Oracle SSO Failure - Unable to process request</h1> </font> Either the requested URL was not specified in terms of a fully-qualified host name or OHS single sign-on is incorrectly configured.<br> Please notify your administrator.<hr> </body>
-</html>
+/*
+ * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oracle.adbaoverjdbc.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author douglas.surber
+ */
+public class NewEmptyJUnitTest {
+  
+  public NewEmptyJUnitTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+
+  // TODO add test methods here.
+  // The methods must be annotated with annotation @Test. For example:
+  //
+  // @Test
+  // public void hello() {}
+}
