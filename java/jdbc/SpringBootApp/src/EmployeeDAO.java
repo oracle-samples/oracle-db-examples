5df04afbb1d2374306b1e1117e34b367a9a7b9ce
nirmala_sundarappa <nirmala.sundarappa@oracle.com>
2020-04-16 16:09:02 -0700

    Revert "Remove SpringBootApp"
    This reverts commit 9bc6f626d2c9b5ba37ccaea386970546a22df8c8.

---
+++ java/jdbc/SpringBootApp/src/EmployeeDAO.java
@@ -0,0 +1,62 @@
+
+/**
+ * Simple DAO class implementation for EMP table.
+ *
+ */
+public class EmployeeDAO {
+	private int empno;
+	private String name;
+	private String job;
+	private int mgr;
+	private int sal;
+	private int deptno;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+}
