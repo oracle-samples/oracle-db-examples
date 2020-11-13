0b844a7a762c51c0b7e4eb9154fbda095b98c25d
Atig Purohit <itsthechamp0074@gmail.com>
2020-11-13 10:11:50 +0530

    Java code to calculate the salary hike

--- java/HRWebApp/src/main/resources/SalaryHikeSP.java
+++ java/HRWebApp/src/main/resources/SalaryHikeSP.java
@@ -7,10 +7,8 @@
              Make sure to have SalaryHikeSP.sql accessible on the 
              client side to execute. 
      Step 2: Run the SQL file after connecting to DB "@SalaryHikeSP.sql" 
-
    NOTES
     Use JDK 1.6 and above
-
    MODIFIED    (MM/DD/YY)
     nbsundar    03/23/15 - Creation (kmensah - Contributor)
  */
@@ -32,8 +30,15 @@ public class SalaryHikeSP {
    */
   public static ResultSet incrementSalary (int percentIncrease) throws SQLException {
     int updateCount = 0,totalemployees = 0,tier3emp = 0,tier2emp = 0,tier1emp = 0;    
-    float totalsalary = 0.0f,minsalary = 0.0f,maxsalary = 0.0f,totalbudget = 0.0f,tier3hike = 0.0f,tier2hike = 0.0f,tier1hike = 0.0f;
-    
+    float totalsalary = 0.0f;
+    float minsalary = 0.0f;
+    float maxsalary = 0.0f;
+    float totalbudget = 0.0f;
+    float tier3hike = 0.0f;
+    float tier2hike = 0.0f;
+    float tier1hike = 0.0f;
+
+
     // Percentage to divide the salary hike budget into
     // three tiers based on the salary range
     // Tier 1 salary range is 15,001 to 25,000
@@ -191,5 +196,3 @@ public class SalaryHikeSP {
     return rset;
   }
 }
-
-
