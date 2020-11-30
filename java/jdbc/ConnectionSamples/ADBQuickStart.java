c47165b1ce1ef7eccfc557dcb0bab9f6498876d9
Nirmala Sundarappa <nirmala.sundarappa@oracle.com>
2020-11-30 01:24:00 -0800

    Update ADBQuickStart.java
    Addressed review comments

--- java/jdbc/ConnectionSamples/ADBQuickStart.java
+++ java/jdbc/ConnectionSamples/ADBQuickStart.java
@@ -46,34 +46,32 @@ import java.util.Scanner;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
 import oracle.ucp.jdbc.PoolDataSource;
 
-public class ADBQuickStart {
+/*
+ * The sample demonstrates connecting to Autonomous Database using 
+ * Oracle JDBC driver and UCP as a client side connection pool.
+ */
+public class ADBQuickStart {  
  
-  // Make sure to have Oracle JDBC release 18c or higher 
-  // to pass TNS_ADMIN as part of a connection URL.
-  // TNS_ADMIN - Should be the path where the client credentials zip file is downloaded. 
-  // wallet_dbname - This is the TNS Alias that you can get from tnsnames.ora.
-  final static String DB_URL="jdbc:oracle:thin:@wallet_dbname?TNS_ADMIN=/Users/test/wallet_dbname/";
-  // Update the Database Username to point to your Autonomous Database. For a quick testing, you can 
-  // use "admin" user. 
-  final static String DB_USER = "admin";
-  // For security reasons, you will need to provide database password through console.  
-  static String DB_PASSWORD ;
-  final static String CONN_FACTORY_CLASS_NAME="oracle.jdbc.pool.OracleDataSource";
-
-  /*
-   * The sample demonstrates UCP as client side connection pool.
-   */
   public static void main(String args[]) throws Exception {
+    // Make sure to have Oracle JDBC driver 18c or above 
+    // to pass TNS_ADMIN as part of a connection URL.
+    // TNS_ADMIN - Should be the path where the client credentials zip file is downloaded. 
+    final String DB_URL="jdbc:oracle:thin:@dbname_medium?TNS_ADMIN=/Users/test/wallet_dbname/";
+    // Update the Database Username and Password to point to your Autonomous Database
+    final String DB_USER = "admin";
+    String DB_PASSWORD = null ;
+    final String CONN_FACTORY_CLASS_NAME="oracle.jdbc.pool.OracleDataSource";
+    
     // For security purposes, you must enter the password through the console 
     try {
       Scanner scanner = new Scanner(System.in);
       System.out.print("Enter the password for Autonomous Database: ");
       DB_PASSWORD = scanner.nextLine();
     }
-    catch (Exception e) {
+    catch (Exception e) {    
        System.out.println("ADBQuickStart - Exception occurred : " + e.getMessage());
-    }
-    
+       System.exit(1);
+    } 
     // Get the PoolDataSource for UCP
     PoolDataSource pds = PoolDataSourceFactory.getPoolDataSource();
 
@@ -96,36 +94,43 @@ public class ADBQuickStart {
     // connections allowed on the connection pool.
     pds.setMaxPoolSize(20);
 
+
     // Get the database connection from UCP.
     try (Connection conn = pds.getConnection()) {
       System.out.println("Available connections after checkout: "
           + pds.getAvailableConnectionsCount());
       System.out.println("Borrowed connections after checkout: "
           + pds.getBorrowedConnectionsCount());
+          
       // Perform a database operation
-      doSQLWork(conn);
+      try {
+        doSQLWork(conn);
+      } catch (SQLException e) {
+          System.out.println("ADBQuickStart - "
+            + "doSQLWork()- SQLException occurred : " + e.getMessage());
+       } 
     }
     
     System.out.println("Available connections after checkin: "
         + pds.getAvailableConnectionsCount());
     System.out.println("Borrowed connections after checkin: "
         + pds.getBorrowedConnectionsCount());
-  }  
-
-  /*
-   * Selects 20 rows from the SH (Sales History) Schema that is the accessible to all 
-   * the database users of autonomous database. 
-   */
-  public static void doSQLWork(Connection conn) {
-  
-    try {
-      conn.setAutoCommit(false);
-      // Prepare a statement to execute the SQL Queries.
-      try (Statement statement = conn.createStatement() ) {
+  }
+ /*
+ * Selects 20 rows from the SH (Sales History) Schema that is the accessible to all 
+ * the database users of autonomous database. 
+ */
+ private static void doSQLWork(Connection conn) throws SQLException {
+    String queryStatement = "SELECT CUST_ID, CUST_FIRST_NAME, CUST_LAST_NAME, CUST_CITY," 
+      + "CUST_CREDIT_LIMIT FROM SH.CUSTOMERS WHERE ROWNUM < 20 order by CUST_ID";
+      
+    System.out.println("Query is " + queryStatement);
+    
+    conn.setAutoCommit(false);
+    // Prepare a statement to execute the SQL Queries.
+    try (Statement statement = conn.createStatement(); 
       // Select 20 rows from the CUSTOMERS table from SH schema. 
-      try (ResultSet resultSet = statement.executeQuery ("select CUST_ID,"  
-          +  "CUST_FIRST_NAME, CUST_LAST_NAME, CUST_CITY, CUST_CREDIT_LIMIT "
-          +  "FROM SH.CUSTOMERS WHERE ROWNUM < 20 order by CUST_ID ") ) {
+      ResultSet resultSet = statement.executeQuery(queryStatement)) {
         System.out.println("\nCUST_ID" + " " + "CUST_FIRST_NAME" + " " + "CUST_LAST_NAME" 
         + " " + "CUST_CITY" + " " + "CUST_CREDIT_LIMIT");
         System.out.println("-----------------------------------------------------------");
@@ -134,16 +139,8 @@ public class ADBQuickStart {
            resultSet.getString(3)+ " " + resultSet.getString(4) + " " +
            resultSet.getInt(5));
         }
-        System.out.println("\nSuccessfully established a connection to Autonomous Database\n");
-       }
-      }
-    }
-    catch (SQLException e) {
-      System.out.println("ADBQuickStart - "
-          + "doSQLWork()- SQLException occurred : " + e.getMessage());
-    } 
-  } // doSQLWork
- 
-} // ADBQuickStart 
-
-    
+      System.out.println("\nSuccessfully established a connection to Autonomous Database\n");
+      } 
+  } // End of doSQLWork
+  
+} // End of ADBQuickStart
