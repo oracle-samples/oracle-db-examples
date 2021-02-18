df3e1fe4575e1f7de9f07ccc30d96f208b196903
Nirmala Sundarappa <nirmala.sundarappa@oracle.com>
2021-02-18 02:30:03 -0800

    Update CreateUser.java

--- java/jdbc/ConnectionSamples/CreateUser.java
+++ java/jdbc/ConnectionSamples/CreateUser.java
@@ -1,4 +1,7 @@
-/* Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.*/
+/* Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+Licensed under the Universal Permissive License v 1.0 
+as shown at http://oss.oracle.com/licenses/upl
+*/
 
 /*
  DESCRIPTION
