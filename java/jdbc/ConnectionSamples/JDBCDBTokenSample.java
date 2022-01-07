8858d9112e33db2434e9a35840bacab5b0a09560
Nirmala Sundarappa <nirmala.sundarappa@oracle.com>
2022-01-07 12:29:59 -0800

    Update JDBCDBTokenSample.java

--- java/jdbc/ConnectionSamples/JDBCDBTokenSample.java
+++ java/jdbc/ConnectionSamples/JDBCDBTokenSample.java
@@ -42,7 +42,7 @@ public class JDBCDBTokenSample {
   final static String DB_URL="jdbc:oracle:thin:@(description="
            + "(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-phoenix-1.oraclecloud.com))"
            + "(security=(token_auth=OCI_TOKEN))"
-           + "(connect_data=(service_name=gebqqeredfsozhjbqbs_dbname_medium.adb.oraclecloud.com)))?oracle.jdbc.tokenAuthentication=OCI_TOKEN";
+           + "(connect_data=(service_name=gebqqeredfsozhjbqbs_dbname_medium.adb.oraclecloud.com)))";
 
 
   public static void main(String args[]) throws SQLException {
