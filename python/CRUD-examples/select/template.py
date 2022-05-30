a28db85f8d80d30f1bb8f085e7e1ede1a5a11946
Christopher Jones <christopher.jones@oracle.com>
2022-05-31 08:37:38 +1000

    It's time for this to be removed

--- python/CRUD-examples/select/template.py
+++
@@ -1,9 +0,0 @@
-# Code Sample from the tutorial at https://learncodeshare.net/2015/06/02/select-crud-using-cx_oracle/
-# The following code is used as the base template for the other examples.
-
-import cx_Oracle
-import os
-connectString = os.getenv('DB_CONNECT') # The environment variable for the connect string: DB_CONNECT=user/password@database
-con = cx_Oracle.connect(connectString)
-
-# Your code here
