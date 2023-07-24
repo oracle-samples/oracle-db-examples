f4064e4f5b1d5a42bff6bbf294150c18e685341e
Salma Elbekraoui <salma.bekraoui86@gmail.com>
2023-07-24 12:49:36 +0100

    Update cleanup.sql

--- javascript/MLE-Demo/cleanup.sql
+++ javascript/MLE-Demo/cleanup.sql
@@ -1,5 +1,32 @@
+DECLARE
+  l_acl_name VARCHAR2(100);
+  CURSOR c_network_acls IS
+    SELECT acl FROM dba_network_acls WHERE host = '*.googleapis.com';
 BEGIN
-  EXECUTE IMMEDIATE 'DROP PROCEDURE SET_WALLET';
-  EXECUTE IMMEDIATE 'DROP TABLE INVENTORY';
-  EXCEPTION WHEN OTHERS THEN NULL;
+  -- Drop the INVENTORY table
+  BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE INVENTORY';
+    DBMS_OUTPUT.PUT_LINE('Table INVENTORY dropped successfully.');
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      DBMS_OUTPUT.PUT_LINE('Table INVENTORY does not exist.');
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+  END;
+
+  -- Find the ACL name for host '*.googleapis.com'
+  OPEN c_network_acls;
+  FETCH c_network_acls INTO l_acl_name;
+
+  -- Process each row returned by the cursor
+  WHILE c_network_acls%FOUND LOOP
+    -- Unassign the ACL
+    DBMS_NETWORK_ACL_ADMIN.UNASSIGN_ACL(acl => l_acl_name);
+    FETCH c_network_acls INTO l_acl_name;
+  END LOOP;
+
+  CLOSE c_network_acls;
+  
+  COMMIT;
 END;
+/
