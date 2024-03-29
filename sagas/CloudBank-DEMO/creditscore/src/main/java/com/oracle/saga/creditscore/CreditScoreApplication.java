1f5536161ed96fa780ff5cc9c16c357e0e8d4715
vinay_pandhariwal <vinay.pandhariwal@oracle.com>
2024-03-29 12:47:14 -0700

    Update Copyright and License

--- sagas/CloudBank-DEMO/creditscore/src/main/java/com/oracle/saga/creditscore/CreditScoreApplication.java
+++ sagas/CloudBank-DEMO/creditscore/src/main/java/com/oracle/saga/creditscore/CreditScoreApplication.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 Oracle and/or its affiliates.
+ * Copyright (c) 2024 Oracle and/or its affiliates.
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
 package com.oracle.saga.creditscore;
--- sagas/CloudBank-DEMO/creditscore/src/main/java/com/oracle/saga/creditscore/listener/AccessControlResponseFilter.java
+++ sagas/CloudBank-DEMO/creditscore/src/main/java/com/oracle/saga/creditscore/listener/AccessControlResponseFilter.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2024 Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
 package com.oracle.saga.creditscore.listener;
 
 import jakarta.annotation.Priority;
