a645f591bd56b0696a7b58b6b0d3bf6590ef15a5
Salma Elbekraoui <salma.elbekraoui@oracle.com>
2024-06-12 15:45:51 +0000

    Change to use declarative types with @graphql-tools/schema

--- javascript/MLE-Demo/book-schema.mjs
+++ javascript/MLE-Demo/book-schema.mjs
@@ -1,4 +1,4 @@
-/*Copyright 2023 Oracle and/or its affiliates.
+/*Copyright 2024 Oracle and/or its affiliates.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,151 +13,88 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {
-    GraphQLSchema,
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLFloat,
-    GraphQLList
-  } from 'graphql';
-  import 'mle-js-fetch'; 
-  import oracledb from 'mle-js-oracledb';
-  
-  const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
-  
-  const getBooks = async (SEARCH_URL) => {
+import { makeExecutableSchema } from 'graphql-tools-schema';
+import 'mle-js-fetch';
+import oracledb from 'mle-js-oracledb';
+
+const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+const typeDefs = `
+  type ImageLinks {
+    thumbnail: String
+  }
+
+  type Author {
+    name: String
+  }
+
+  type VolumeInfo {
+    title: String
+    subtitle: String
+    publisher: String
+    description: String
+    authors: [Author]
+    publishedDate: String
+    imageLinks: ImageLinks
+    averageRating: Float
+    pageCount: Int
+    language: String
+  }
+
+  type Book {
+    id: String
+    kind: String
+    etag: String
+    volumeInfo: VolumeInfo
+    stock: Int
+  }
+
+  type Volumes {
+    kind: String
+    totalItems: Int
+    items: [Book]
+  }
+
+  type Query {
+    library(research: String): Volumes
+  }
+`;
+
+const resolvers = {
+  Query: {
+    library: async (_, { research }) => {
+      return await getBooks(research);
+    }
+  },
+  VolumeInfo: {
+    authors: (volumeInfo) => volumeInfo.authors || [],
+  },
+  Book: {
+    stock: async (book) => {
+      const conn = oracledb.defaultConnection();
+      const result = await conn.execute('SELECT stock FROM inventory WHERE id = :id', [book.id], { outFormat: oracledb.OUT_FORMAT_OBJECT });
+      if (result.rows.length > 0) {
+        return result.rows[0].STOCK;
+      } else {
+        return 0;
+      }
+    }
+  },
+};
+
+const getBooks = async (SEARCH_URL) => {
   const conn = oracledb.defaultConnection();
-  conn.execute(`
-  begin
-     utl_http.set_wallet('file:/<absolute-path-to-your-wallet-directory>');
-  end;
+  await conn.execute(`
+    begin
+      utl_http.set_wallet('file:/<absolute-path-to-your-wallet-directory>');
+    end;
   `);
-  
   const response = await fetch(`${BASE_URL}${SEARCH_URL}`, { credentials: "include" });
   const result = await response.json();
   return result;
-  }
-  
-  const imageLinks = new GraphQLObjectType({
-    name: 'IMAGE_LINKS',
-    fields: () => ({
-        thumbnail: { 
-            type: GraphQLString, 
-        },
-    })
-  });
-  
-  const author = new GraphQLObjectType({
-  name: 'AUTHOR',
-  fields: () => ({
-    name: { 
-      type: GraphQLString,
-      resolve: (author) => author || "Unknown",
-    },
-  })
-  });
-  
-  const volumeInfo = new GraphQLObjectType({
-  name: 'VOLUME_INFO',
-  fields: () => ({
-    title: {
-        type: GraphQLString,
-    },
-    subtitle: {
-        type: GraphQLString,
-    },
-    publisher: {
-        type: GraphQLString,
-    },
-    description: {
-        type: GraphQLString,
-    },
-    authors: {
-        type: new GraphQLList(author),
-        resolve: (volumeInfo) => volumeInfo.authors || [],
-    },
-    publishedDate: {
-        type: GraphQLString,
-    },
-    imageLinks: {
-        type: imageLinks,
-    },
-    averageRating: {
-        type: GraphQLFloat,
-    },
-    pageCount: {
-        type: GraphQLInt,
-    },
-    language: {
-        type: GraphQLString,
-    }
-  })
-  });
-  
-  const book = new GraphQLObjectType({
-    name: 'BOOK',
-    fields: () => ({
-      id: {
-        type: GraphQLString,
-        description: 'The id of the book.',
-      },
-      kind: {
-        type: GraphQLString,
-        description: 'The kind of the book.',
-      },
-      etag: {
-        type: GraphQLString,
-        description: 'The etag of the book.',
-      },
-      volumeInfo: {
-          type: volumeInfo,
-      },
-      stock: {
-          type: GraphQLInt,
-          resolve: (book) => {
-              const result = session.execute('select stock from inventory where id = :id', [book.id]);
-              if (result.rows.length > 0) {
-                  return result.rows[0].STOCK;
-              } else {
-                  return 0;
-              }
-          }
-      },
-    })
-  });
-  
-  const volumes = new GraphQLObjectType({
-  name: 'VOLUMES',
-  fields: () => ({
-    kind: { 
-        type: GraphQLString, 
-    },
-    totalItems: { 
-        type: GraphQLInt, 
-    },
-    items: { 
-        type: new GraphQLList(book), 
-    },
-  }),
-  });
-  
-  const queryType = new GraphQLObjectType({
-      name: 'Query',
-      fields: () => ({
-        library: {
-            args: {
-                research: {
-                    type: GraphQLString,
-                },
-            },
-            type: volumes,
-            resolve: async (_source, {research}) => await getBooks(research),
-        },
-      }),
-  });
-  
-  export const schema = new GraphQLSchema({
-    query: queryType,
-    types: [volumes, book, volumeInfo, author, imageLinks]
-  });
+};
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
