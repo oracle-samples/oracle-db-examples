79adfc510a6c2b8a5be761d17cb998f9c4277e9c
juarezjuniorgithub <juarez.barbosa@oracle.com>
2024-07-02 00:34:49 +0100

    additional adjustments requested by Jean

--- java/springboot-hikari-ucp/src/main/java/com/oracle/dev/jdbc/ucp/spring/EmployeeApplication.java
+++ java/springboot-hikari-ucp/src/main/java/com/oracle/dev/jdbc/ucp/spring/EmployeeApplication.java
@@ -27,8 +27,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeApplication.class, args);
+  }
 
 }
