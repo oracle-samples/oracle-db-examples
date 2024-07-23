494d184bc65b059ca1471959b950ff1b7965083f
Emmanuel Jannetti <4819607+ejannett@users.noreply.github.com>
2024-07-23 12:31:21 +0200

    Signed-off-by: Emmanuel Jannetti <emmanuel.jannetti@oracle.com>
            Oracle JDBC SQL statement interceptor integration.
            fix copyrights

--- java/jdbc/statement-interceptor/interceptor/src/main/java/com/oracle/jdbc/samples/interceptor/rules/TokenStatementRule.java
+++ java/jdbc/statement-interceptor/interceptor/src/main/java/com/oracle/jdbc/samples/interceptor/rules/TokenStatementRule.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates.
+ *
+ *   This software is dual-licensed to you under the Universal Permissive License
+ *   (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License
+ *   2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose
+ *   either license.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ */
+
 package com.oracle.jdbc.samples.interceptor.rules;
 
 /**
