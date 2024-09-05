e0956a342db156831daf9382deef17bec90e854f
Paulo Alberto Simoes <paulo.simoes@oracle.com>
2024-09-05 11:42:55 -0300

    Add OKafka Transactional Examples and Update Kafka Client (#390)

--- txeventq/okafka/Quickstart/Transactional/Consumer/src/main/java/org/oracle/okafka/examples/TransactionalConsumerOKafka.java
+++ txeventq/okafka/Quickstart/Transactional/Consumer/src/main/java/org/oracle/okafka/examples/TransactionalConsumerOKafka.java
@@ -7,6 +7,9 @@
 
 package org.oracle.okafka.examples;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import java.sql.Connection;
 import java.time.Duration;
@@ -24,68 +27,32 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import org.oracle.okafka.clients.consumer.KafkaConsumer;
 
-
 public class TransactionalConsumerOKafka {
 
-    // Dummy implementation of ConsumerRebalanceListener interface
-    // It only maintains the list of assigned partitions in assignedPartitions list
-    static class ConsumerRebalance implements ConsumerRebalanceListener {
-
-        public List<TopicPartition> assignedPartitions = new ArrayList();
-
-        @Override
-        public synchronized void onPartitionsAssigned(Collection<TopicPartition>  partitions) {
-            System.out.println("Newly Assigned Partitions:");
-            for (TopicPartition tp :partitions ) {
-                System.out.println(tp);
-                assignedPartitions.add(tp);
-            }
-        }
-
-        @Override
-        public synchronized void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-            System.out.println("Revoked previously assigned partitions. ");
-            for (TopicPartition tp :assignedPartitions ) {
-                System.out.println(tp);
-            }
-            assignedPartitions.clear();
-        }
-    }
-
     public static void main(String[] args) {
-        Properties props = new Properties();
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
 
-        // Option 1: Connect to Oracle Database with database username and password
-        props.put("security.protocol","PLAINTEXT");
-        //IP or Host name where Oracle Database 23ai is running and Database Listener's Port
-        props.put("bootstrap.servers", "localhost:1521");
-        props.put("oracle.service.name", "freepdb1"); //name of the service running on the database instance
-        // location for ojdbc.properties file where user and password properties are saved
-        props.put("oracle.net.tns_admin",".");
+        // Get application properties
+        Properties appProperties = null;
+        try {
+            appProperties = getProperties();
+            if (appProperties == null) {
+                System.out.println("Application properties not found!");
+                System.exit(-1);
+            }
+        } catch (Exception e) {
+            System.out.println("Application properties not found!");
+            System.out.println("Exception: " + e);
+            System.exit(-1);
+        }
 
-		/*
-		//Option 2: Connect to Oracle Autonomous Database using Oracle Wallet
-		//This option to be used when connecting to Oracle autonomous database instance on OracleCloud
-		props.put("security.protocol","SSL");
-		// location for Oracle Wallet, tnsnames.ora file and ojdbc.properties file
-		props.put("oracle.net.tns_admin",".");
-		props.put("tns.alias","Oracle23ai_high");
-		*/
+        String topicName = appProperties.getProperty("topic.name", "TXEQ");
+        appProperties.remove("topic.name"); // Pass props to build OKafkaProducer
 
-        //Consumer Group Name
-        props.put("group.id" , "CG1");
-        props.put("enable.auto.commit","false");
-
-        // Maximum number of records fetched in single poll call
-        props.put("max.poll.records", 10);
-
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-        Consumer<String , String> consumer = new KafkaConsumer<String, String>(props);
+        Consumer<String , String> consumer = new KafkaConsumer<String, String>(appProperties);
         ConsumerRebalanceListener rebalanceListener = new ConsumerRebalance();
 
-        consumer.subscribe(Arrays.asList("TXEQ"), rebalanceListener);
+        consumer.subscribe(Arrays.asList(topicName), rebalanceListener);
 
         int expectedMsgCnt = 100;
         int msgCnt = 0;
@@ -151,5 +118,56 @@ public class TransactionalConsumerOKafka {
     private static void processRecord(Connection conn, ConsumerRecord<String, String> record)
     {
         //Application specific logic to process the message
+    }
+
+
+    private static java.util.Properties getProperties()  throws IOException {
+        InputStream inputStream = null;
+        Properties appProperties;
+
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+            inputStream = TransactionalConsumerOKafka.class.getClassLoader().getResourceAsStream(propFileName);
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found.");
+            }
+            appProperties = prop;
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            throw e;
+        } finally {
+            if (inputStream != null)
+                inputStream.close();
+        }
+        return appProperties;
+    }
+
+    // Dummy implementation of ConsumerRebalanceListener interface
+    // It only maintains the list of assigned partitions in assignedPartitions list
+    static class ConsumerRebalance implements ConsumerRebalanceListener {
+
+        public List<TopicPartition> assignedPartitions = new ArrayList();
+
+        @Override
+        public synchronized void onPartitionsAssigned(Collection<TopicPartition>  partitions) {
+            System.out.println("Newly Assigned Partitions:");
+            for (TopicPartition tp :partitions ) {
+                System.out.println(tp);
+                assignedPartitions.add(tp);
+            }
+        }
+
+        @Override
+        public synchronized void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            System.out.println("Revoked previously assigned partitions. ");
+            for (TopicPartition tp :assignedPartitions ) {
+                System.out.println(tp);
+            }
+            assignedPartitions.clear();
+        }
     }
 }
--- txeventq/okafka/Quickstart/Transactional/Producer/src/main/java/org/oracle/okafka/examples/TransactionalProducerOKafka.java
+++ txeventq/okafka/Quickstart/Transactional/Producer/src/main/java/org/oracle/okafka/examples/TransactionalProducerOKafka.java
@@ -7,90 +7,111 @@
 
 package org.oracle.okafka.examples;
 
-import org.oracle.okafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.errors.DisconnectException;
 import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.KafkaException;
 
+import org.oracle.okafka.clients.producer.KafkaProducer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TransactionalProducerOKafka {
+
     public static void main(String[] args) {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+
+        // Get application properties
+        Properties appProperties = null;
+        try {
+            appProperties = getProperties();
+            if (appProperties == null) {
+                System.out.println("Application properties not found!");
+                System.exit(-1);
+            }
+        } catch (Exception e) {
+            System.out.println("Application properties not found!");
+            System.out.println("Exception: " + e);
+            System.exit(-1);
+        }
+
+
+        boolean failProcessing = false;
+        if ((args != null) && (args.length == 1)) {
+            failProcessing = args[0].equals("FAIL");
+        }
+
+        String topicName = appProperties.getProperty("topic.name", "TXEQ");
+        appProperties.remove("topic.name"); // Pass props to build OKafkaProducer
+
+
         Producer<String, String> producer = null;
         try {
-            Properties props = new Properties();
-
-            // Option 1: Connect to Oracle Database with database username and password
-            props.put("security.protocol","PLAINTEXT");
-            //IP or Host name where Oracle Database 23ai is running and Database Listener's Port
-            props.put("bootstrap.servers", "localhost:1521");
-            props.put("oracle.service.name", "freepdb1"); //name of the service running on the database instance
-            // location for ojdbc.properties file where user and password properties are saved
-            props.put("oracle.net.tns_admin",".");
-			
-			/*
-			//Option 2: Connect to Oracle Autonomous Database using Oracle Wallet
-			//This option to be used when connecting to Oracle autonomous database instance on OracleCloud
-			props.put("security.protocol","SSL");
-			// location for Oracle Wallet, tnsnames.ora file and ojdbc.properties file
-			props.put("oracle.net.tns_admin","."); 
-			props.put("tns.alias","Oracle23ai_high"); 
-			*/
-
-            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-            //Property to create a Transactional Producer
-            props.put("oracle.transactional.producer", "true");
-
-            producer = new KafkaProducer<String, String>(props);
+            producer = new KafkaProducer<String, String>(appProperties);
 
             int msgCnt = 100;
-            String jsonPayload = "{\"name\":\"Programmer"+msgCnt+"\",\"status\":\"classy\",\"catagory\":\"general\",\"region\":\"north\",\"title\":\"programmer\"}";
+            int limitMessages = 33;
+
+            String jsonPayload = "{\"name\":\"Programmer"+msgCnt+"\",\"status\":\"classy\",\"catagory\":\"general\"," +
+                    "\"region\":\"north\",\"title\":\"programmer\"}";
+
             System.out.println(jsonPayload);
             producer.initTransactions();
 
             Connection conn = ((KafkaProducer<String, String> )producer).getDBConnection();
-            String topicName = "TXEQ";
+
             // Produce 100 records in a transaction and commit.
             try {
                 producer.beginTransaction();
-                boolean fail = false;
-                for( int i=0;i<msgCnt;i++) {
+                AtomicBoolean fail = new AtomicBoolean(false);
+                int messagesProduced = 0;
+                while (messagesProduced < msgCnt) {
                     //Optionally set RecordHeaders
                     RecordHeader rH1 = new RecordHeader("CLIENT_ID", "FIRST_CLIENT".getBytes());
-                    RecordHeader rH2 = new RecordHeader("REPLY_TO", "TXEQ_2".getBytes());
+                    RecordHeader rH2 = new RecordHeader("REPLY_TO", (topicName.concat("_RETURN")).getBytes());
 
                     ProducerRecord<String, String> producerRecord =
-                            new ProducerRecord<String, String>(topicName, i+"", jsonPayload);
+                            new ProducerRecord<String, String>(topicName, String.valueOf(messagesProduced), jsonPayload);
                     producerRecord.headers().add(rH1).add(rH2);
+
                     try {
-                        processRecord(conn, producerRecord);
+                        processRecord(conn, producerRecord, failProcessing, messagesProduced, limitMessages);
                     } catch(Exception e) {
-                        //Retry processRecord or abort the Okafka transaction and close the producer
-                        fail = true;
+                        System.out.println(e.getMessage());
+                        //Retry processRecord or abort the OKafka transaction and close the producer
+                        fail.set(true);
                         break;
                     }
+
                     producer.send(producerRecord);
+                    messagesProduced++;
                 }
 
-                if(fail) // Failed to process the records. Abort Okafka transaction
-                    producer.abortTransaction();
-                else // Successfully process all the records. Commit OKafka transaction
-                    producer.commitTransaction();
+                String returnMsg = String.format("Produced %d messages.", messagesProduced);
 
-                System.out.println("Produced 100 messages.");
-            }catch( DisconnectException dcE) {
+                if(fail.get()) {   // Failed to process the records. Abort Okafka transaction
+                    producer.abortTransaction();
+                    System.out.println(returnMsg.concat(" Process aborted and not committed."));
+
+                } else {          // Successfully process all the records. Commit OKafka transaction
+                    producer.commitTransaction();
+                    System.out.println(returnMsg);
+                }
+
+            } catch( DisconnectException dcE) {
                 producer.close();
-            }catch (KafkaException e) {
+            } catch (KafkaException e) {
                 producer.abortTransaction();
             }
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             System.out.println("Exception in Main " + e );
             e.printStackTrace();
         }
@@ -98,8 +119,7 @@ public class TransactionalProducerOKafka {
             try {
                 if(producer != null)
                     producer.close();
-            }catch(Exception e)
-            {
+            } catch(Exception e) {
                 System.out.println("Exception while closing producer " + e);
                 e.printStackTrace();
 
@@ -108,9 +128,40 @@ public class TransactionalProducerOKafka {
         }
     }
 
-    private static void processRecord(Connection conn, ProducerRecord<String, String> record) throws Exception
+    private static void processRecord(Connection conn, ProducerRecord<String, String> record,
+                                      boolean failProcessing,
+                                      int messagesProduced, int limitMessages) throws Exception
     {
         //Application specific logic
+
+        // Samples logic to fail during execution and abort the transaction
+        if ((failProcessing) && (messagesProduced == limitMessages))
+            throw new Exception("Exception while processing record");
+    }
+
+    private static java.util.Properties getProperties()  throws IOException {
+        InputStream inputStream = null;
+        Properties appProperties;
+
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+            inputStream = TransactionalProducerOKafka.class.getClassLoader().getResourceAsStream(propFileName);
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found.");
+            }
+            appProperties = prop;
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            throw e;
+        } finally {
+            if (inputStream != null)
+                inputStream.close();
+        }
+        return appProperties;
     }
 
 }
