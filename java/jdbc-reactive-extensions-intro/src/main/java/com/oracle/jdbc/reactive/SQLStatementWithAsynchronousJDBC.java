4c48f678035756d492ddc188efecb01fd2a2ef23
juarezjuniorgithub <juarez.barbosa@oracle.com>
2025-03-06 10:27:04 +0000

    updated to driver 23.7.0.25.01, added Statements for DDL, added OraclePreparedStatement for DML, added new queries including select (streaming) example method, added sql file, added DB connection config.properties file

--- java/jdbc-reactive-extensions-intro/src/main/java/com/oracle/jdbc/reactive/SQLStatementWithAsynchronousJDBC.java
+++ java/jdbc-reactive-extensions-intro/src/main/java/com/oracle/jdbc/reactive/SQLStatementWithAsynchronousJDBC.java
@@ -23,75 +23,156 @@ package com.oracle.jdbc.reactive;
 
 import java.sql.SQLException;
 import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+import java.util.stream.IntStream;
 
 import oracle.jdbc.OracleConnection;
 import oracle.jdbc.OraclePreparedStatement;
-import oracle.jdbc.pool.OracleDataSource;
+import oracle.jdbc.OracleResultSet;
+import oracle.jdbc.OracleStatement;
 
 public class SQLStatementWithAsynchronousJDBC {
 
-  public static void main(String[] args) throws SQLException {
-    OracleDataSource ods = new OracleDataSource();
-    // jdbc:oracle:thin@[hostname]:[port]/[DB service/name]
-    ods.setURL("jdbc:oracle:thin@[hostname]:[port]/[DB service/name");
-    ods.setUser("[Username]");
-    ods.setPassword("[Password]");
-    OracleConnection conn = (OracleConnection) ods.getConnection();
+  public static void main(String[] args) throws InterruptedException {
     SQLStatementWithAsynchronousJDBC asyncSQL = new SQLStatementWithAsynchronousJDBC();
-    // Execute a SQL DDL statement to create a database table
-    // asynchronously
-    asyncSQL.createTable(conn);
+    try (OracleConnection conn = DatabaseConfig.getConnection()) {
+      asyncSQL.createTable(conn);
+      IntStream.rangeClosed(0, 3).forEach(i -> asyncSQL.insertData(conn, i, "Java " + i, "Duke " + i));
+      asyncSQL.readData(conn);
+      Thread.sleep(5000);
+      asyncSQL.dropTable(conn);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
-  /**
-   * Asynchronously creates a new table by executing a DDL SQL statement
-   * 
-   * @param connection
-   *          Connection to a database where the table is created
-   * @return A Publisher that emits the result of executing DDL SQL
-   * @throws SQLException
-   *           If a database access error occurs before the DDL SQL can be
-   *           executed
-   */
-  private Flow.Publisher<Boolean> createTable(OracleConnection connection)
-      throws SQLException {
+  private Flow.Publisher<Boolean> insertData(OracleConnection connection, int id, String firstName, String lastName) {
+    try {
+      final OraclePreparedStatement insertStatement = (OraclePreparedStatement) connection
+          .prepareStatement("INSERT INTO employee_names (id, first_name, last_name) VALUES (?, ?, ?)");
+      insertStatement.setInt(1, id);
+      insertStatement.setString(2, firstName);
+      insertStatement.setString(3, lastName);
 
-    OraclePreparedStatement createTableStatement = (OraclePreparedStatement) connection
-        .prepareStatement(
-            "CREATE TABLE employee_names (" + "id NUMBER PRIMARY KEY, "
-                + "first_name VARCHAR(50), " + "last_name VARCHAR2(50))");
+      Flow.Publisher<Boolean> insertPublisher = insertStatement.unwrap(OraclePreparedStatement.class)
+          .executeAsyncOracle();
 
-    Flow.Publisher<Boolean> createTablePublisher = createTableStatement
-        .unwrap(OraclePreparedStatement.class).executeAsyncOracle();
+      insertPublisher.subscribe(new Flow.Subscriber<Boolean>() {
+        private Flow.Subscription subscription;
 
-    createTablePublisher.subscribe(
-        // This subscriber will close the PreparedStatement
-        new Flow.Subscriber<Boolean>() {
-          public void onSubscribe(Flow.Subscription subscription) {
-            subscription.request(1L);
-          }
+        public void onSubscribe(Flow.Subscription subscription) {
+          this.subscription = subscription;
+          subscription.request(1L);
+        }
 
-          public void onNext(Boolean item) {
-          }
+        public void onNext(Boolean item) {
+        }
 
-          public void onError(Throwable throwable) {
-            closeStatement();
-          }
+        public void onError(Throwable throwable) {
+          closeStatement();
+          throwable.printStackTrace();
+        }
 
-          public void onComplete() {
-            closeStatement();
-          }
+        public void onComplete() {
+          closeStatement();
+        }
 
-          void closeStatement() {
-            try {
-              createTableStatement.close();
-            } catch (SQLException closeException) {
-              closeException.printStackTrace();
+        void closeStatement() {
+          try {
+            if (insertStatement != null && !insertStatement.isClosed()) {
+              insertStatement.close();
             }
+          } catch (SQLException closeException) {
+            closeException.printStackTrace();
           }
-        });
+        }
+      });
 
-    return createTablePublisher;
+      return insertPublisher;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      SubmissionPublisher<Boolean> publisher = new SubmissionPublisher<>();
+      publisher.close();
+      return publisher;
+    }
   }
 
+  public Flow.Publisher<OracleResultSet> readData(OracleConnection connection) {
+    try {
+      final OraclePreparedStatement readStatement = (OraclePreparedStatement) connection
+          .prepareStatement("SELECT * FROM employee_names WHERE first_name LIKE ?");
+      readStatement.setString(1, "Jav%");
+
+      Flow.Publisher<OracleResultSet> readPublisher = readStatement.unwrap(OraclePreparedStatement.class)
+          .executeQueryAsyncOracle();
+
+      readPublisher.subscribe(new Flow.Subscriber<OracleResultSet>() {
+        private Flow.Subscription subscription;
+
+        public void onSubscribe(Flow.Subscription subscription) {
+          this.subscription = subscription;
+          subscription.request(Long.MAX_VALUE);
+        }
+
+        public void onNext(OracleResultSet resultSet) {
+          try {
+            while (resultSet.next()) {
+              int id = resultSet.getInt("id");
+              String firstName = resultSet.getString("first_name");
+              String lastName = resultSet.getString("last_name");
+              System.out.println("ID: " + id + ", First Name: " + firstName + ", Last Name: " + lastName);
+            }
+            System.out.println("Finished receiving stream data successfully. \nPreparing to drop table...");
+          } catch (SQLException e) {
+            onError(e);
+          }
+        }
+
+        public void onError(Throwable throwable) {
+          closeStatement();
+          throwable.printStackTrace();
+        }
+
+        public void onComplete() {
+          closeStatement();
+        }
+
+        void closeStatement() {
+          try {
+            if (readStatement != null && !readStatement.isClosed()) {
+              readStatement.close();
+            }
+          } catch (SQLException closeException) {
+            closeException.printStackTrace();
+          }
+        }
+      });
+      return readPublisher;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      SubmissionPublisher<OracleResultSet> publisher = new SubmissionPublisher<>();
+      publisher.close();
+      return publisher;
+    }
+  }
+
+  private void createTable(OracleConnection connection) {
+    String createTableSQL = "CREATE TABLE employee_names (id NUMBER PRIMARY KEY, first_name VARCHAR2(50), last_name VARCHAR2(50))";
+    try (OracleStatement createTableStatement = (OracleStatement) connection.createStatement()) {
+      createTableStatement.execute(createTableSQL);
+      System.out.println("Table 'employee_names' created successfully.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void dropTable(OracleConnection connection) {
+    String dropTableSQL = "DROP TABLE employee_names";
+    try (OracleStatement dropTableStatement = (OracleStatement) connection.createStatement()) {
+      dropTableStatement.execute(dropTableSQL);
+      System.out.println("Table 'employee_names' dropped successfully.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
